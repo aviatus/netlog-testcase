d4ccbdcc7244ba15138e0b8fe85c623f9d762ea9
ahmet <ahmet.yildiz@btechanalytics.net>
2021-09-04 22:49:46 +0300

    Fix shared components exports

---
+++ src/app/shared/components/index.ts
@@ -0,0 +1,14 @@
+export * from './chart/chart.component';
+export * from './chart/chart.module';
+
+export * from './daterangepicker/daterangepicker.component';
+export * from './daterangepicker/daterangepicker.module';
+
+export * from './dropdown/dropdown.component';
+export * from './dropdown/dropdown.module';
+
+export * from './table/table.component';
+export * from './table/table.module';
+
+export * from './autocomplete/autocomplete.component';
+export * from './autocomplete/autocomplete.module';
