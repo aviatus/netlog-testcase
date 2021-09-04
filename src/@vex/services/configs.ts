81ef9db5a96d7182d72bde55be95ffd73e3e6c96
ahmet <ahmet.yildiz@btechanalytics.net>
2021-09-04 22:44:42 +0300

    Fix sidenav element name

--- src/@vex/services/configs.ts
+++ src/@vex/services/configs.ts
@@ -1,6 +1,6 @@
-import { mergeDeep } from '../utils/merge-deep';
 import { ConfigName } from '../interfaces/config-name.model';
 import { Config } from '../interfaces/config.model';
+import { mergeDeep } from '../utils/merge-deep';
 
 const defaultConfig: Config = {
   id: ConfigName.apollo,
@@ -9,7 +9,7 @@ const defaultConfig: Config = {
   layout: 'horizontal',
   boxed: false,
   sidenav: {
-    title: 'VEX',
+    title: 'Fon Analizi',
     imageUrl: 'assets/img/demo/logo.svg',
     showCollapsePin: true,
     state: 'expanded'
