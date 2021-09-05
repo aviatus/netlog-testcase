b94fd4ed09fdea1276e67e2eaaaac7cbb64cbf07
ahmet <ahmet.yildiz@btechanalytics.net>
2021-09-06 00:48:48 +0300

    Add missing imports in toolbar module

--- src/@vex/layout/toolbar/toolbar.module.ts
+++ src/@vex/layout/toolbar/toolbar.module.ts
@@ -1,21 +1,23 @@
 import { BreadcrumbsModule } from 'src/@vex/components/breadcrumbs/breadcrumbs.module';
+
 import { CommonModule } from '@angular/common';
-import { ToolbarComponent } from './toolbar.component';
+import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
+import { MatRippleModule } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatRippleModule } from '@angular/material/core';
-import { ToolbarNotificationsModule } from './toolbar-notifications/toolbar-notifications.module';
-import { ToolbarUserModule } from './toolbar-user/toolbar-user.module';
-import { ToolbarSearchModule } from './toolbar-search/toolbar-search.module';
-import { IconModule } from '@visurel/iconify-angular';
-import { NavigationModule } from '../navigation/navigation.module';
 import { RouterModule } from '@angular/router';
-import { NavigationItemModule } from '../../components/navigation-item/navigation-item.module';
-import { MegaMenuModule } from '../../components/mega-menu/mega-menu.module';
-import { ContainerModule } from '../../directives/container/container.module';
+import { IconModule } from '@visurel/iconify-angular';
 
+import { MegaMenuModule } from '../../components/mega-menu/mega-menu.module';
+import { NavigationItemModule } from '../../components/navigation-item/navigation-item.module';
+import { ContainerModule } from '../../directives/container/container.module';
+import { NavigationModule } from '../navigation/navigation.module';
+import { ToolbarNotificationsModule } from './toolbar-notifications/toolbar-notifications.module';
+import { ToolbarSearchModule } from './toolbar-search/toolbar-search.module';
+import { ToolbarUserModule } from './toolbar-user/toolbar-user.module';
+import { ToolbarComponent } from './toolbar.component';
 
 @NgModule({
   declarations: [ToolbarComponent],
