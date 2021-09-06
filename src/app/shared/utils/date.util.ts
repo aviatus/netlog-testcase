070678879a4cd3eef69eb157a7ac699912d028ed
ahmet <ahmet.yildiz@btechanalytics.net>
2021-09-06 22:06:13 +0300

    Add description to date util

--- src/app/shared/utils/date.util.ts
+++ src/app/shared/utils/date.util.ts
@@ -1,7 +1,13 @@
 import moment from 'moment';
 
 export class DateUtil {
-  static setDateToString(date: Date) {
+
+  /**
+  * Transform date to YYYY-MM-DD format
+  * @param date The date value to transform.
+  * @returns The transformed date string.
+  */
+  static formatDateToYYYYMMDD(date: Date) {
     return moment(date).format('YYYY-MM-DD');
   }
 }
